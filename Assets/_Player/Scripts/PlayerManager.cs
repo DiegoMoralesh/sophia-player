2b617f14a7deb14bc389b6618c05bce6ba3ce683
Jissel Rodriguez <jrodriguez@edutecno.com>
2023-10-06 15:16:34 -0400

    commit

--- Assets/_Player/Scripts/PlayerManager.cs
+++ Assets/_Player/Scripts/PlayerManager.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
+//using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
