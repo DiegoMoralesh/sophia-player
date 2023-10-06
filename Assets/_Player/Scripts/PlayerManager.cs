be13abad3370fc61f12b641b3b4cce56fd30025e
DiegoMoralesh <125034331+DiegoMoralesh@users.noreply.github.com>
2023-10-06 10:47:06 -0300

    Funcionalidad de Palabras Claves

--- Assets/_Player/Scripts/PlayerManager.cs
+++ Assets/_Player/Scripts/PlayerManager.cs
@@ -3,6 +3,7 @@ using Doozy.Engine.UI;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -79,7 +80,7 @@ namespace SophiaPlayer
             var value = RandomEnumValue<ThemeManager.LayoutEnum>();
 
             GameObject s = GameObject.Instantiate((UnityEngine.GameObject)Resources.Load("Layout/Style1/"+value.ToString()), Vector3.zero, Quaternion.identity, transform);
-            s.GetComponent<Slide>().setData(sd);
+            s.GetComponent<Slide>().setData(sd, KeyPhrase(sd));
             slides.Add(s);
         }
 
@@ -88,5 +89,97 @@ namespace SophiaPlayer
             var v = Enum.GetValues(typeof(T));
             return (T)v.GetValue(_R.Next(v.Length));
         }
+
+        List<double> KeyPhrase(SlideData sd)
+        {
+            List<double> segPhrase = new List<double>(); 
+            for (int x = 0; x < sd.keyPhrase.Length; x++)
+            {
+                string[] phraseSplit = sd.keyPhrase[x].Split(' ');
+                Similarity s = new Similarity();
+                s.grade = 0;
+                s.text = "";
+                s.seg = 0;
+
+                for (int i = 0; i < sd.srt.Length; i++)
+                {  
+                    if (sd.srt[i].text.Equals(phraseSplit[0], StringComparison.OrdinalIgnoreCase))
+                    {
+                        string aux = "";
+                        for (int e = 0; e < phraseSplit.Length; e++)
+                        {
+                            aux += sd.srt[i + e].text+" ";
+                        }
+
+                        double similarity = CalculateSimilarity(aux, sd.keyPhrase[x]);
+                        double similarityThreshold = 0.3;
+
+                        if (similarity >= similarityThreshold)
+                        {
+                            if(s.grade < similarity)
+                            {
+                                s.grade = similarity;
+                                s.text = sd.keyPhrase[x];
+                                s.seg = sd.srt[i].start_time;
+                            }
+                        }
+                    }
+                }
+
+                segPhrase.Add(s.seg);
+            }
+            return segPhrase;
+        }
+
+        public double CalculateSimilarity(string string1, string string2)
+        {
+            int string1Length = string1.Length;
+            int string2Length = string2.Length;
+
+            int[,] distanceMatrix = new int[string1Length + 1, string2Length + 1];
+
+            for (int i = 0; i <= string1Length; i++)
+            {
+                distanceMatrix[i, 0] = i;
+            }
+
+            for (int j = 0; j <= string2Length; j++)
+            {
+                distanceMatrix[0, j] = j;
+            }
+
+            for (int i = 1; i <= string1Length; i++)
+            {
+                for (int j = 1; j <= string2Length; j++)
+                {
+                    int cost = (string1[i - 1] == string2[j - 1]) ? 0 : 1;
+                    distanceMatrix[i, j] = Math.Min(
+                        Math.Min(distanceMatrix[i - 1, j] + 1, distanceMatrix[i, j - 1] + 1),
+                        distanceMatrix[i - 1, j - 1] + cost
+                    );
+                }
+            }
+
+            double distance = distanceMatrix[string1Length, string2Length];
+            double maxLength = Math.Max(string1Length, string2Length);
+
+            return 1.0 - (distance / maxLength);
+        }
+
+        [SerializeField] GameObject prefabKeyPhrase;
+            
+        public void ShowKeyPhrase(Transform content, string phrase)
+        {
+            TMP_Text TMPtext = GameObject.Instantiate(prefabKeyPhrase, content).GetComponent<TMP_Text>();
+            TMPtext.text = phrase;
+        }
     }
+}
+
+[System.Serializable]
+public struct Similarity
+{
+    public double grade;
+    public string text;
+    public double seg;
 }
